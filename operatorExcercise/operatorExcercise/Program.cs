4a232da709e5a4fdef068927578fd7419979a504
Michael Scharffbillig <mscharffbillig@gmail.com>
2022-06-20 20:22:08 -0500

    added AreaOfCircle method to the program

--- operatorExcercise/operatorExcercise/Program.cs
+++ operatorExcercise/operatorExcercise/Program.cs
@@ -79,6 +79,17 @@ namespace operatorExcercise
                 Console.WriteLine("Invalid entry. run program again.");
                 Console.ReadLine();
             }
+
+
+            Console.WriteLine("Lets calculate the area of a circle...");
+            Console.Write("What is the radius? ");
+             
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+            Console.ReadLine();
+
+
+
         }
 
         public static int Add(int num1, int num2)
@@ -99,11 +110,26 @@ namespace operatorExcercise
         public static int Divide(int num1, int num2)
         {
             int sum = num1 / num2;
-            
+
             return sum;
         }
-    }
 
+        public static double AreaOfCircle(double radius)
+        {
+            double result = Math.PI * (radius * radius);
+
+            return result;
+        }
+
+
+    }
 }
+
+    
+
+
+
+
+
     
 
